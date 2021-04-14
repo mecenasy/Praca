80c49922ca18ea3c180cc82af237886683a72a46
Marcin <mecenasy@gmail.com>
2021-04-14 11:27:02 +0200

    update Menu Shema

--- api/src/Models/Menu.ts
+++ api/src/Models/Menu.ts
@@ -1,15 +1,20 @@
 import { Schema, Document, model } from "mongoose";
 
-export interface IMenu extends Document {
+export interface IMenu {
    name: string;
+   shortName?: string;
    link: string;
    image: string;
 }
 
-const menuSchema = new Schema<IMenu>({
+const menuSchema = new Schema<IMenu & Document>({
    name: String,
+   shortName: {
+      type: String,
+      required: false,
+   },
    link: String,
    image: String,
 });
 
-export default model<IMenu>('menu', menuSchema);
+export default model<IMenu & Document>('menu', menuSchema);
